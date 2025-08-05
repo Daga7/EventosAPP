c79e138ed3cef7646c04baea8f3a79cb50fc1944
Daga7 <dagaeda77@gmail.com>
2025-08-04 23:25:55 -0500

    Agregando gett y sett y corrigiendo errores

--- EventosAPP/src/java/eventosAPP/model/Usuario.java
+++ EventosAPP/src/java/eventosAPP/model/Usuario.java
@@ -1,57 +1,123 @@
 package eventosAPP.model;
 import java.util.ArrayList;
+import java.time.LocalDate;
 import java.util.List;
 
-//Atributos Usuarios
-
+//Clase  Usuarios
 public class Usuario {
-    private int id;
-    private String Nombres;
-    private String Apellidos;
-    private String Email;
-    private String Contrasena;
-    private String Telefono;
-    private String fechaNacimiento;    
-    private String Rol;
-    private ArrayList<Evento> eventos;
-    private ArrayList<Notificacion> notificaciones;
-    private ArrayList<Mensaje> mensajes;
+    private int usuarioID;
+    private String nombres;
+    private String apellidos;
+    private String email;
+    private String contrasena;
+    private String telefono;
+    private LocalDate fechaNacimiento  
+    private String rol;
+    private ArrayList<Eventos> eventos;
+    private ArrayList<Notificaciones> notificaciones;
+    private ArrayList<Mensajes> mensajes;
 
     // Constructor de la clase Usuario
-    public Usuario(String nombres, String apellidos, String correo, String contraseña,String telefono, String rol, String fechaNacimiento) {
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-        this.correo = correo;
-        this.contraseña = contraseña;
+    public Usuario( int usuarioID , String nombres, String apellidos, String email , String contrasena,String telefono,Sring fechaNacimiento, String rol) {
+        this.usuarioID = usuarioID;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.contrasena = contrasena;
         this.telefono = telefono;
-        this.rol = rol;
         this.fechaNacimiento = fechaNacimiento;
+        this.Rol = Rol;
         this.eventos = new ArrayList<>();
         this.notificaciones = new ArrayList<>();
         this.mensajes = new ArrayList<>();
     }
-        public String getNombres() { return nombres; }
-        public void setNombres(String nombres) { this.nombres = nombres; }
 
-        public String getApellidos() { return apellidos; }
-        public void setApellidos(String apellidos) { this.apellidos = apellidos; }
+    // getters y setters
 
-        public String getCorreo() { return correo; }
-        public void setCorreo(String correo) { this.correo = correo; }
+        //gett Y sett usuarioID
+        public String getUsuarioID() {
+            return usuarioID;
+        }
+        public void setUsuraioID(int usuarioId){
+            this.usuarioID =usuarioID;
+        }
+        
+        //gett y sett Nombre
+        public String getNombre() { 
+            return nombre; 
+        }
+        public void setNombre(String nombre) { 
+            this.nombre = nombre; 
+        }
+        
+        //gett y sett Apellido
+        public String getApellido() { 
+            return apellido; 
+        }
+        public void setApellido(String apellidos) { 
+            this.apellido = apellido; 
+        }
 
-        public String getContraseña() { return contraseña; }
-        public void setContraseña(String contraseña) { this.contraseña = contraseña; }
+        //gett y sett Email
+        public String getEmail() { 
+            return correo; 
+        }
+        public void setEmail(String email) { 
+            this.email = email; 
+        }
 
-        public String getTelefono() { return telefono; }
-        public void setTelefono(String telefono) { this.telefono = telefono; }
+        //gett y sett contrasena
+        public String getContrasena() { 
+            return contrasena; 
+        }
+        public void setContrasena(String contrasena) { 
+            this.contraseña = contraseña; 
+        }
 
-        public String getRol() { return rol; }
-        public void setRol(String rol) { this.rol = rol; }
+        //gett y sett Telefono
+        public String getTelefono() { 
+            return telefono; 
+        }
+        public void setTelefono(String telefono) { 
+            this.telefono = telefono; 
+        }
 
-        public String getFechaNacimiento() { return fechaNacimiento; }
-        public void setFechaNacimiento(String fechaNacimiento) { this.fechaNacimiento = fechaNacimiento; }
+        //gett y sett FechaNacimiento
+        public String getFechaNacimiento() { 
+            return fechaNacimiento; 
+        }
+        public void setFechaNacimiento(String fechaNacimiento) { 
+            this.fechaNacimiento = fechaNacimiento; 
+        }
 
-        //Metodo para verificar si el usuario es organizador
+        //gett y sett Rol
+        public String getRol() { 
+            return rol; 
+        }
+        public void setRol(String rol) { 
+            this.rol = rol; 
+        }
+
+        //gett Lista eventos
+        public ArrayList<Eventos> geteventos(){
+            return eventos;
+        }
+
+        //get Lista notificaciones 
+        public ArrayList<Notificacions> getnotificaciones(){
+            return notificaciones;
+        }
+
+        //gett Lista mensajes 
+        public ArrayList<Mensajes> getmensajes(){
+            return mensajes;
+        }
+
+
+        //Metodo comportamiento usuario
+
+        //Metodo vetificacion de Rol y permisos 
+            // Arreglar el tema de que pueda cambair el evento 
         public boolean esOrganizador(){
             return this.rol.equalsIgnoreCase("organizador");
         }
